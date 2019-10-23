dabd11310939d66006374a8909ba6d1922c85149
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-10-23 22:14:32 +0300

    Обновления скрипта для кнопки fixet

--- src/blocks/button/button.js
+++ src/blocks/button/button.js
@@ -1,12 +1,14 @@
-var cardsSectionOffset = $('.card-selection').offset().top;
-var firstScreenHeight = $('.banking-services').outerHeight();
+if ( $('.card-selection').length &&  $('.banking-services').length) {
+   var cardsSectionOffset = $('.card-selection').offset().top;
+   var firstScreenHeight = $('.banking-services').outerHeight();
 
-$(window).scroll(function () {
-   var scroll = $(this).scrollTop();
-   if (scroll >= firstScreenHeight && scroll < cardsSectionOffset) {
-      $('.button--fixet').addClass('button--fixet-active')
-   }
-   else {
-      $('.button--fixet').removeClass('button--fixet-active')
-   }
-});
+   $(window).scroll(function () {
+      var scroll = $(this).scrollTop();
+      if (scroll >= firstScreenHeight && scroll < cardsSectionOffset) {
+         $('.button--fixet').addClass('button--fixet-active')
+      }
+      else {
+         $('.button--fixet').removeClass('button--fixet-active')
+      }
+   });
+}
