fad3a3191ba25b4370f8f861e4721c93f4158b91
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-10-29 17:16:11 +0200

    Добавлены новые модальные окна в разметку блока modal, стили для фиксации body и js код

--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -1,22 +1,85 @@
 $(function(){
 
-   var openModal = document.querySelector('.modal-openstatus a');
+   var openModal = document.querySelector('.modal-openstatus__button a');
+   var openModalEmail = document.querySelector('#email-modal');
+   var openModalCallback = document.querySelector('.modal-callback__button a');
+   var openModalAccepted = document.querySelector('.modal-callback__button-send');
    var modal = document.querySelector('.modal');
+   var modalEmail = document.querySelector('.modal-email');
+   var modalOpenstatus = document.querySelector('.modal-openstatus');
+   var modalСallback = document.querySelector('.modal-callback');
+   var modalAccepted = document.querySelector('.modal-accepted');
    var popur = document.querySelector('.popup');
-   var closeModal = document.querySelector('.close');
+   var closeModalOpenstatus = document.querySelector('.modal-openstatus__close');
+   var closeModalEmail = document.querySelector('.modal-email__close');
+   var closeModalСallback = document.querySelector('.modal-callback__close');
+   var closeModalAccepted = document.querySelector('.modal-accepted__close');
+   var btnStatusPage = document.querySelector('.modal-openstatus__form .form-email__button');
+   // var pageHtml = document.querySelector('.page');
 
-   openModal.addEventListener('click', function (e) {
-      e.preventDefault();
-      modal.classList.add('modal--opened');
-      popur.classList.add('popup--opened');
-      document.body.classList.add('hidden');
+
+   // модалка по кнопке с callback
+   openModalAccepted.addEventListener('click', function () {
+      modalAccepted.classList.add('modal-accepted--opened');
+      modalСallback.classList.remove('modal-callback--opened');
+   });
+
+   closeModalAccepted.addEventListener('click', function () {
+      modal.classList.remove('modal--opened');
+      modalAccepted.classList.remove('modal-accepted--opened');
+      document.body.classList.remove('hidden');
    });
 
 
-   closeModal.addEventListener('click', function () {
+   // модалка в кнопке фиксед
+   openModalEmail.addEventListener('click', function () {
+      modal.classList.add('modal--opened');
+      modalEmail.classList.add('modal-email--opened');
+      document.body.classList.add('hidden');
+      // pageHtml.classList.add('hidden');
+   });
+
+   closeModalEmail.addEventListener('click', function () {
       modal.classList.remove('modal--opened');
-      popur.classList.remove('popup--opened');
+      modalEmail.classList.remove('modal-email--opened');
       document.body.classList.remove('hidden');
+      // pageHtml.classList.remove('hidden');
+   });
+
+   // модалка в кнопке в футуре Durumum
+   openModal.addEventListener('click', function (e) {
+      e.preventDefault();
+      modal.classList.add('modal--opened');
+      modalOpenstatus.classList.add('modal-openstatus--opened');
+      document.body.classList.add('hidden');
+      // pageHtml.classList.add('hidden');
+   });
+
+   closeModalOpenstatus.addEventListener('click', function () {
+      modal.classList.remove('modal--opened');
+      modalOpenstatus.classList.remove('modal-openstatus--opened');
+      document.body.classList.remove('hidden');
+      pageHtml.classList.remove('hidden');
+   });
+
+   // модалка в кнопке в футуре callback
+   openModalCallback.addEventListener('click', function (e) {
+      e.preventDefault();
+      modal.classList.add('modal--opened');
+      modalСallback.classList.add('modal-callback--opened');
+      document.body.classList.add('hidden');
+      // pageHtml.classList.add('hidden');
+   });
+
+   closeModalСallback.addEventListener('click', function () {
+      modal.classList.remove('modal--opened');
+      modalСallback.classList.remove('modal-callback--opened');
+      document.body.classList.remove('hidden');
+      // pageHtml.classList.remove('hidden');
+   });
+
+   btnStatusPage.addEventListener('click', function () {
+      window.location.href='status-page.html';
    });
 
 
@@ -27,4 +90,9 @@ $(function(){
       popur.classList.remove('popup--opened');
       document.body.classList.remove('hidden');
    };
+
+   // $(modal).on('touchmove', function(event) {
+   //    event.preventDefault();
+   // });
+
 });
